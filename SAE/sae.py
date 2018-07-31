76a1b2781f5988a5d19dc09f1bc848cb02f3ae07
Jerling <1161676899@qq.com>
2018-07-31 15:30:03 +0800

    在sae中添加了不同的预处理方式，发现标准化正确率略高

--- SAE/sae.py
+++ SAE/sae.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
-sys.path.append("..")
+sys.path.append(".")
+print(sys.path)
 from data.datapath import dataloader
 from scipy.linalg import solve_sylvester
 from sklearn import preprocessing
@@ -18,27 +19,47 @@ def s2f(W, x):
     return np.matmul(W, x.T)
 
 def main():
+    # 加载数据集
     dataset = dataloader()
     dicl = list(dataset.keys())
     for dic in dicl:
         print(dic,': ', dataset[dic].shape)
 
     # preprocessing
+    # stadard
     scaler = preprocessing.StandardScaler().fit(dataset[dicl[-1]])
     train_att = scaler.transform(dataset[dicl[-1]])
     scaler = preprocessing.StandardScaler().fit(dataset[dicl[0]])
     test_att = scaler.transform(dataset[dicl[0]])
 
+    # MinMaxScaler
+    #  train_att = preprocessing.MinMaxScaler().fit_transform(dataset[dicl[-1]])
+    #  test_att = preprocessing.MinMaxScaler().fit_transform(dataset[dicl[0]])
+
+    # Nomalization
+    #  train_att = preprocessing.normalize(dataset[dicl[-1]], norm="l1")
+    #  test_att = preprocessing.normalize(dataset[dicl[0]], norm="l1")
+    #  train_att = preprocessing.normalize(dataset[dicl[-1]], norm="l2")
+    #  test_att = preprocessing.normalize(dataset[dicl[0]], norm="l2")
+
     # cumpute W 
     W = sae(dataset[dicl[-3]].T, train_att.T, lambda_)
     #  print(W.shape)
+
+    # reconstruct s
     s_ = s2f(W, dataset[dicl[1]])
     #  print(s_.shape)
+    
+    # compute consine similarity between s_ and test_att
     dist = cosine(s_.T, test_att)
     #  print(dist.shape)
+
+    # get the index of the most similarity label
     y_ = np.argmax(dist, axis=1)
     #  print(y_.shape)
     #  print(y_)
+
+    # cumpute the accuracy of testSet
     print("The accuracy is : ",(np.equal(dataset[dicl[2]], dataset[dicl[-2]][y_])).mean())
 
 
