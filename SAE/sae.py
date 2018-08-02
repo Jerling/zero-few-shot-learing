9ebc1d2e068c52b447823262906a9be43c03b5df
Jerling <1161676899@qq.com>
2018-08-02 15:18:43 +0800

    加入kears的Dense，效果不佳，不知为何。

--- SAE/sae.py
+++ SAE/sae.py
@@ -2,10 +2,14 @@ import sys
 import numpy as np
 sys.path.append(".")
 print(sys.path)
-from data.datapath import dataloader
+from data.datasets import dataloader
 from scipy.linalg import solve_sylvester
 from sklearn import preprocessing
 from sklearn.metrics.pairwise import cosine_similarity as cosine
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense,Dropout
+from keras.optimizers import SGD
 
 lambda_ = .2
 
@@ -18,12 +22,50 @@ def sae(x , s, lambda_):
 def s2f(W, x):
     return np.matmul(W, x.T)
 
+def labeltoindex(y):
+    dicty_tr_ = {}
+    idx = 0
+    list_y = []
+    for data in y:
+        data = data[0]
+        if data not in dicty_tr_.keys():
+            dicty_tr_[data] = idx
+            idx += 1
+        list_temp = np.zeros(50)
+        list_temp[dicty_tr_[data]] = 1.0
+        list_y.append(list_temp)
+    return np.array(list_y)
+
+def nlpway(x_train, y_train):
+    y_train = labeltoindex(y_train)
+    print('x_train :', x_train.shape)
+    print('y_train :', y_train.shape)
+    model =  Sequential()
+    model.add(Dense(312, activation='relu', input_dim=312))
+    model.add(Dropout(0.5))
+    model.add(Dense(50, activation='softmax'))
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer = sgd,
+                  metrics = ['accuracy'])
+    model.fit(x_train, y_train,
+              epochs=10,
+              batch_size=8)
+
+    return model.evaluate(x_train[:500], y_train[:500],batch_size=8)
+
 def main():
     # 加载数据集
     dataset = dataloader()
     dicl = list(dataset.keys())
     for dic in dicl:
         print(dic,': ', dataset[dic].shape)
+
+    test_x = dataset[dicl[1]] # (2933, 1024)
+    test_y = dataset[dicl[2]] # (2933, 1)
+
+    x_train, x_test, y_train, y_test = train_test_split(test_x, test_y, test_size=0.3)
 
     # preprocessing
     # stadard
@@ -48,7 +90,7 @@ def main():
 
     # reconstruct s
     s_ = s2f(W, dataset[dicl[1]])
-    #  print(s_.shape)
+    print(s_.shape)
     
     # compute consine similarity between s_ and test_att
     dist = cosine(s_.T, test_att)
@@ -61,6 +103,10 @@ def main():
 
     # cumpute the accuracy of testSet
     print("The accuracy is : ",(np.equal(dataset[dicl[2]], dataset[dicl[-2]][y_])).mean())
+
+    x_train = s2f(W, test_x)
+    score = nlpway(x_train.T, test_y)
+    print('Score : ', score)
 
 
 if __name__ == '__main__':
